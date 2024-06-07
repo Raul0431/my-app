2c1c4ddff069a2eec71c9fd1ebad1f14fc855cdb
danielbatres <danielbatresuser@gmail.com>
2024-06-06 20:52:19 -0600

    Add Invoice screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import { Dashboard } from "app/components/Dashboard";
+import { Invoice } from "app/components/Invoice";
 import { Navbar } from "app/components/Navbar";
 import { Sidebar } from "app/components/Sidebar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
-      <section className="hidden lg:block min-w-[15%]">
-        <Sidebar />
-      </section>
-      <section className="flex flex-col items-center w-full lg:max-w-[85%] min-h-full">
-        <Navbar />
-        <Dashboard />
-      </section>
-    </main>
+    <>
+      <Invoice />
+      <main className="flex min-h-screen">
+        <section className="hidden lg:block min-w-[15%]">
+          <Sidebar />
+        </section>
+        <section className="flex flex-col items-center w-full lg:max-w-[85%] min-h-full">
+          <Navbar />
+          <Dashboard />
+        </section>
+      </main>
+    </>
   );
 }
---
+++ src/components/Invoice.tsx
@@ -0,0 +1,11 @@
+const Invoice = (): JSX.Element => {
+  return (
+    <div className="min-w-full bg-opacity-50 h-screen fixed bg-black flex flex-col lg:items-center lg:justify-center justify-end">
+      <div className="overflow-auto lg:max-w-[50%] max-h-[85%] w-full h-full bg-white rounded-lg">
+        <h4>Texto</h4>
+      </div>
+    </div>
+  );
+}
+
+export { Invoice };
